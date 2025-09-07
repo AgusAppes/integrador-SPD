a35e5441139adbec8c1edc462610ce542654932b
AgusAppes <agustinaappes@gmail.com>
2025-09-07 13:36:22 -0300

    enlace a trello en home

--- views/home.php
+++ views/home.php
@@ -8,7 +8,7 @@
 
 <!-- El home debe mostrar el nombre del boliche y datos como la dirección, etc y tener un botón para iniciar sesión o registrarse --> 
 <!-- Los botones deben ser enlaces a las paginas de login y register -->
-<!-- para ver como armar los enlaces, ver el archivo index.php y la tarjeta de trello -->
+<!-- para ver como armar los enlaces, ver el archivo index.php y la tarjeta de trello https://trello.com/c/4aBlce3V-->
 <body>
     <h1>Bienvenido al Sistema de Tickets</h1> 
     <button><a href="?page=login">Iniciar sesión</a></button>
